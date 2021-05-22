3611383f8e6d98af0773f9a5edb4d7eb7a612fae
tonywet <59941480+tonywet@users.noreply.github.com>
2021-05-23 00:10:03 +0300

    Add files via upload

---
+++ servers2.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+import sqlite3
+
+
+def update_sql(array):
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    login = array[0]
+    pb_key = array[1]
+    cur.execute("UPDATE users SET pb_key= ? WHERE login=? ", (pb_key, login))
+    con.commit()
+    con.close()
+
+
+def get():
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users ")
+    rows = cur.fetchall()
+    print(rows)
+    return rows
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip = socket.gethostbyname(socket.gethostname())
+sock.bind((ip, 2247))
+client = []  # Массив где храним адреса клиентов
+print('Start Server')
+while 1:
+    data, addres = sock.recvfrom(1024)
+    print(data)
+    try:
+        list = data.decode()
+        if list == '^^^':
+            print(1)
+            datas = get()
+            print(datas)
+            datas = str(datas)
+            sock.sendto(datas.encode(), addres)
+        else:
+            opendata = list.split('>')
+            update_sql(opendata)
+    except:
+        if addres not in client:
+            client.append(addres)  # Если такого клиента нету , то добавить
+        for clients in client:
+            if clients == addres:
+                continue  # Не отправлять данные клиенту, который их прислал
+            sock.sendto(data, clients)
+
+
+potok = threading.Thread(target=read_sok)
+potok.start()
+while 1:
+    mensahe = input()
+    sor.sendto(('[' + alias + ']' + mensahe).encode('utf-8'), server)
